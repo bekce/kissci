cc49a4ecb650ad66534e7d8c9437364452f6c88a
bekce <seb@sebworks.com>
2018-10-31 17:20:45 +0100

    Minor

--- prepare.sh
+++ prepare.sh
@@ -5,7 +5,7 @@ SHA=$2
 BUILD_SH_PATH=$3
 # create tmp dir clone repo
 rnd1=${RANDOM}${RANDOM}
-mkdir -p runs
+mkdir -p runs logs
 git clone $CLONE_ADDRESS runs/$rnd1
 cd runs/$rnd1
 git checkout $SHA
